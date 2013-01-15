b2a6737e220efcec709dde0bfeb1ed486048b7a4
Caleb Jares <cable729@gmail.com>
2013-01-15 09:45:48 -0600

    Changed arrays to be implicitly typed. Verified there is no difference so I don't look like a fool: http://stackoverflow.com/questions/14341097/what-is-the-difference-between-new-and-new-string

--- Resume/CalebJares.cs
+++ Resume/CalebJares.cs
@@ -32,26 +32,26 @@ namespace Resume
                 Description = "Back-end architecture lead for www.locabal.com. Locabal is a " +
                     "student-led startup whose mission is to connect producers of handcrafted items " +
                     "with buyers both locally and globally.",
-                Technologies = new string[] { "C#", ".NET", "MVC", "EntityFramework" }
+                Technologies = new[] { "C#", ".NET", "MVC", "EntityFramework" }
             },
             Intel = new {
                 Start = new DateTime(2012, 5, 14), End = new DateTime(2012, 8, 10),
                 Description = "Used PowerShell and various cmd-line tools to automate installations " +
                     "and updates of server instances. Reverse-engineered 3rd-party installers that " +
                     "didn't offer automated installers.",
-                Technologies = new string[] { "PowerShell", "Windows", "cmd" }
+                Technologies = new[] { "PowerShell", "Windows", "cmd" }
             },
             Green_Bein = new {
                 Start = new DateTime(2012, 3, 8), End = new DateTime(2012, 5, 1),
                 Description = "Worked on the game Synch, a 2D platformer that allows users " +
                     "to share custom created stages through Facebook and Synch's website.",
-                Technologies = new string[] { "Actionscript 3" }
+                Technologies = new[] { "Actionscript 3" }
             },
             UNL_Avery_Hall = new {
                 Start = new DateTime(2011, 11, 11), End = new DateTime(2012, 5, 1),
                 Description = "Helped students with computer science homework and provided "+
                     "technical maintanence for the CSE department.",
-                Technologies = new string[] { "C++", "Java", "Matlab", "Unix", "Algorithms" }
+                Technologies = new[] { "C++", "Java", "Matlab", "Unix", "Algorithms" }
             }
         };
 
