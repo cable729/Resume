6b4e7d24c9259958f16844f6d6870bd536e2a2cb
Caleb Jares <cable729@gmail.com>
2013-01-14 18:44:23 -0600

    Extracted interfaces.

---
+++ Resume/Interfaces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume
+{
+    public interface IHirable { IEnumerable<string> Skills { get; } }
+    public interface IPassionate { IEnumerable<string> Interests { get; } }
+    public interface INetDeveloper /* : IDisposable // ha ha */ { }
+    public interface IStudent { }
+}
--- Resume/Program.cs
+++ Resume/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 namespace Resume
 {
-    /// <see cref="http://github.com/cable729"/>
-    public class CalebJares //: INetDeveloper, IStudent, IPassionate, IHirable, IEmploymentHistory
+    /// <see cref="https://github.com/cable729/Resume"/>
+    public class CalebJares : INetDeveloper, IStudent, IPassionate, IHirable
     {
         public dynamic ContactInformation = new {
             Email = new Uri(@"cjares@cse.unl.edu"), Phone = "970-590-3046"
@@ -17,7 +17,7 @@ namespace Resume
 
         public IEnumerable<string> Interests { get { return new string[] {
             "C#", "Software Architecture", ".NET", "Dependency Injection & IoC",
-            "MVC 4", "C++", "Mobile Development", "Git", "TFS" };
+            "MVC 4", "C++", "Game Development", "Mobile Development", "Git", "TFS" };
         } }
 
         public IEnumerable<string> Skills { get { return new string[] {
@@ -25,32 +25,32 @@ namespace Resume
             "MVC", "MVVM", "XML", "Java", "C++", "LaTeX", "Html", "CSS", "Javascript", "UNIX" };
         } }
 
-        public IEnumerable<EmploymentRecord> PreviousWork { get { return new List<EmploymentRecord> {
-            new EmploymentRecord {
-                Name = "Locabal", Start = new DateTime(2012, 8, 24), CurrentlyEmployed = true,
+        public dynamic PreviousWork { get { return new {
+            Locabal = new {
+                Start = new DateTime(2012, 8, 24), CurrentlyEmployed = true,
                 Description = "Back-end architecture lead for www.locabal.com. Locabal is a " +
                     "student-led startup whose mission is to connect producers of handcrafted items " +
                     "with buyers both locally and globally.",
-                Technologies = new string[] { "C#", "MVC", "EntityFramework" }
+                Technologies = new string[] { "C#", ".NET", "MVC", "EntityFramework" }
             },
-            new EmploymentRecord {
-                Name = "Intel", Start = new DateTime(2012, 5, 14), End = new DateTime(2012, 8, 10),
+            Intel = new {
+                Start = new DateTime(2012, 5, 14), End = new DateTime(2012, 8, 10),
                 Description = "Used PowerShell and various cmd-line tools to automate installations " +
                     "and updates of server instances. Reverse-engineered 3rd-party installers that " +
                     "didn't offer automated installers.",
                 Technologies = new string[] { "PowerShell", "Windows", "cmd" }
             },
-            new EmploymentRecord {
-                Name = "Green Bein", Start = new DateTime(2012, 3, 8), End = new DateTime(2012, 5, 1),
+            Green_Bein = new {
+                Start = new DateTime(2012, 3, 8), End = new DateTime(2012, 5, 1),
                 Description = "Worked on the game Synch, a 2D platformer that allows users " +
                     "to share custom created stages through Facebook and Synch's website.",
                 Technologies = new string[] { "Actionscript 3" }
             },
-            new EmploymentRecord {
-                Name = "UNL Avery Hall", Start = new DateTime(2011, 11, 11), End = new DateTime(2012, 5, 1),
+            UNL_Avery_Hall = new {
+                Start = new DateTime(2011, 11, 11), End = new DateTime(2012, 5, 1),
                 Description = "Helped students with computer science homework and provided "+
                     "technical maintanence for the CSE department.",
-                Technologies = new string[] { "C++, Java, Matlab, Unix, Algorithms, Discrete Mathematics" }
+                Technologies = new string[] { "C++", "Java", "Matlab", "Unix", "Algorithms" }
             } };
         } }
 
@@ -58,25 +58,5 @@ namespace Resume
             Name = "University of Nebraska-Lincoln", Majors = new[] { "Computer Science", "Mathematics" },
             GPA = "3.0", Start = new DateTime(2011, 8, 17), Graduation = new DateTime(2015, 5, 5)
         };
-
-        internal event EventHandler OnHire;
-        public bool Hire(EventArgs hireArgs) { this.OnHire(this, hireArgs); return true; }
-
-        public static void Main(string[] args) { }
     }
-
-    public class EmploymentRecord
-    {
-        public string Name { get; set; }
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
-        public bool CurrentlyEmployed { get; set; }
-        public string Description { get; set; }
-        public IEnumerable<string> Technologies { get; set; }
-    }
-    public interface IEmploymentHistory { IEnumerable<EmploymentRecord> PreviousWork { get; } }
-    public interface IHirable { IEnumerable<string> Skills { get; } }
-    public interface IPassionate { IEnumerable<string> Interests { get; } }
-    public interface INetDeveloper /* : IDisposable // ha ha */ { }
-    public interface IStudent { }
 }
